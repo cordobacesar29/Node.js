3a60a9a32fe14d351ff8e4c07cf98f457d34d218
Cesar Cordoba <cordobacesar29@gmail.com>
2021-02-16 18:01:56 -0300

    metodo put para editar y delete para API

--- enrutador.js
+++ enrutador.js
@@ -4,7 +4,7 @@ module.exports = {
     },
     mascotas:{
         get: (data, callback) => {
-            if(data.indice) {
+            if(typeof data.indice !== 'undefined') {
                 if(global.recursos.mascotas[data.indice]) {
                     return callback(200, global.recursos.mascotas[data.indice]);
                 }
@@ -17,7 +17,34 @@ module.exports = {
         post: (data, callback) => {
             global.recursos.mascotas.push(data.payload);
             callback(201, data.payload);            
-        }
+        },
+        put: (data, callback) => {
+            if(typeof data.indice !== 'undefined') {
+                if(global.recursos.mascotas[data.indice]) {
+                    global.recursos.mascotas[data.indice] = data.payload;
+                    return callback(200, global.recursos.mascotas[data.indice]);
+                }
+                return callback(404, {
+                    mensaje:`mascota con indice${data.indice} no encontrada`,
+                });
+            }
+            callback(400, {mensaje: 'indice no enviado'});
+        },
+        delete: (data, callback) => {
+            if(typeof data.indice !== 'undefined') {
+                if(global.recursos.mascotas[data.indice]) {
+                    global.recursos.mascotas = global.recursos.mascotas.filter(
+                    (_mascota, indice) => indice != data.indice);
+                    return callback(204, {
+                        mensaje: `el elemento con indice ${data.indice} fue eliminado`
+                    });
+                }
+                return callback(404, {
+                    mensaje:`mascota con indice${data.indice} no encontrada`,
+                });
+            }
+            callback(400, {mensaje: 'indice no enviado'});
+        },
     },
     noEncontrado: (data, callback) => {
         callback(404,{mensaje: 'no encontrado enrutador'});
--- recursos.js
+++ recursos.js
@@ -1,8 +1,8 @@
 module.exports = { 
     mascotas :[
-        {tipo: 'Perro', nombre: 'Firulais', propietario:'Cesar'},
-        {tipo: 'Perro', nombre: 'Firulais', propietario:'Cesar'},
-        {tipo: 'Perro', nombre: 'Firulais', propietario:'Cesar'},
-        {tipo: 'Perro', nombre: 'Firulais', propietario:'Cesar'},
-        {tipo: 'Perro', nombre: 'Firulais', propietario:'Cesar'}
+        {tipo: 'Perro', nombre: 'Firulais0', propietario:'Cesar'},
+        {tipo: 'Perro', nombre: 'Firulais1', propietario:'Cesar'},
+        {tipo: 'Perro', nombre: 'Firulais2', propietario:'Cesar'},
+        {tipo: 'Perro', nombre: 'Firulais3', propietario:'Cesar'},
+        {tipo: 'Perro', nombre: 'Firulais4', propietario:'Cesar'}
 ]}
