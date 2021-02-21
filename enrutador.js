7e77411fd8b4c43d97a69c1fdc565af608f6edd9
Cesar Cordoba <cordobacesar29@gmail.com>
2021-02-21 16:50:55 -0300

    ruta profesionales

--- enrutador.js
+++ enrutador.js
@@ -1,11 +1,13 @@
 const recursos = require('./recursos');
 const mascotas = require('./rutas/mascotas');
+const profesionales = require('./rutas/profesionales');
 
 module.exports = {
     ruta: (data, callback) => {
         callback(200, {mensaje: 'esta es la ruta desde enrutador'});
     },
     mascotas: mascotas(recursos.mascotas),
+    profesionales: profesionales(recursos.profesionales),
     noEncontrado: (data, callback) => {
         callback(404,{mensaje: 'no encontrado enrutador'});
     }
--- recursos.js
+++ recursos.js
@@ -5,4 +5,10 @@ module.exports = {
         {tipo: 'Perro', nombre: 'Firulais2', propietario:'Cesar'},
         {tipo: 'Perro', nombre: 'Firulais3', propietario:'Cesar'},
         {tipo: 'Perro', nombre: 'Firulais4', propietario:'Cesar'}
-]}
+    ],
+    profesionales :[
+        {nombre: 'Alexandra', Apellido:'Pérez', DNI: '36696941'},
+        {nombre: 'Ayelen', Apellido:'Pérez', DNI: '65223448'},
+        {nombre: 'Cesar', Apellido:'ura', DNI: '12156546'}
+    ]    
+};
